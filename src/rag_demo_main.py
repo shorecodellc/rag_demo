2c9675a88fde55b54e565d49992b9a513d640e12
Kevin Fink <kevin@shorecode.org>
2026-04-06 09:49:55 +0700

    init

---
+++ src/rag_demo_main.py
@@ -0,0 +1,18 @@
+#kevin fink
+#kevin@shorecode.org
+#Mon Apr  6 09:49:55 AM +07 2026
+#
+
+import sys
+from  import
+from  import
+
+files = Files()
+filepaths = files.get_files_list()
+log_fp = filepaths[0]
+
+logger = set_logging('', log_fp)
+
+if __name__ == '__main__':
+    sys.exit(0)
+
